2e17e8cf36b6985e725a03a718f6f8e84164aaf9
SamuelGabriel <muelsamu@student.ethz.ch>
2016-04-30 12:05:18 +0200

    Added Today Widget, with best restaurant

---
+++ SaltToday/TodayViewController.h
@@ -0,0 +1,12 @@
+//
+//  TodayViewController.h
+//  SaltToday
+//
+//  Created by Samuel Mueller on 29.04.16.
+//  Copyright © 2016 HACKETH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface TodayViewController : UIViewController
+
+@end
---
+++ SaltToday/TodayViewController.m
@@ -0,0 +1,68 @@
+//
+//  TodayViewController.m
+//  SaltToday
+//
+//  Created by Samuel Mueller on 29.04.16.
+//  Copyright © 2016 HACKETH. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+#import <CoreLocation/CoreLocation.h>
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+#define AF_APP_EXTENSIONS
+@interface TodayViewController () <NCWidgetProviding>
+@property (strong, nonatomic) IBOutletCollection(UIImageView) NSArray *imV;
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    
+    // Perform any setup necessary in order to update the view.
+    CLLocationManager *locmanager = [[CLLocationManager  alloc]init];
+    
+    NSString *address = [NSString stringWithFormat:@"http://www-saltapp.rhcloud.com/restaurants/discover?lat=%f&long=%f",locmanager.location.coordinate.latitude,locmanager.location.coordinate.longitude];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET: address parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSArray *sortedArray = [((NSArray *)((NSDictionary *)responseObject)[@"restaurants"]) sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            return [(NSString *)[obj1 valueForKey:@"rating"] floatValue]<[(NSString *)[obj2 valueForKey:@"rating"] floatValue];
+        }];
+        self.nameLabel.text = [NSString stringWithFormat:@"%@ is around the corner",sortedArray[0][@"name"]];
+        for (int i =0; i<4; i++) {
+            [(UIImageView *)_imV[i] setImageWithURL:[NSURL URLWithString:sortedArray[0][@"photos"][i][@"small_url"]]];
+        }
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"tError: %@", error);
+        
+    }];
+    
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+    
+    completionHandler(NCUpdateResultNewData);
+}
+
+
+
+@end
